3cd356db68ba654a507e05922bb002ac879f5926
Alexey <sysoev84l@ya.ru>
2021-04-25 18:42:14 +0300

    chrome-extensions

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,15 +1,14 @@
-import {applyMiddleware, combineReducers} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
-import musicReducer from "./music-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
+import musicReducer from "./music-reducer";
 import thunkMiddleware from "redux-thunk";
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
 import appReducer from "./app-reducer";
 
-const {createStore} = require("redux");
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -21,6 +20,8 @@ let reducers = combineReducers({
     app: appReducer,
     form: formReducer
 });
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-window.store = store;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunkMiddleware)));
+window.__store__ = store;
 export default store
